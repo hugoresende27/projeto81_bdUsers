8360556d8191b54fa7c4ce32f373bfdb0f673522
hugoresende27 <Hugoresende27@gmail.com>
2021-12-05 21:42:02 +0000

    a

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    /////////////////////////////////////interface 
+    echo '<h3>gestão de users</h3>';
+    echo '<hr>';
+
+
+    /////////////////////////////////////navegação
+    echo '<a href="">Adicionar User</a>';
+
+    /////////////////////////////////// tabela com users existentes
+
+
+    /////////////////////////////////////incluir o sistema de gestão
+    include 'gestor.php';
+    $gestor = new Gestor();
+
+
+    ///////////////////////////////////// buscar dados de users registados
+    $resultados = $gestor->EXE_QUERY("SELECT * FROM users");
+    ///////////////////////////////////// apresentar resultados numa tabela
+    echo count($resultados);
+    echo '<pre>';
+    print_r($resultados);
+
+?>
