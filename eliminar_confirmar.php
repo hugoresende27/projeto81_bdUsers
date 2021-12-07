c2068993d687ebbb408b41d197de5f5d252844a1
hugoresende27 <Hugoresende27@gmail.com>
2021-12-07 23:09:34 +0000

    Update eliminar_confirmar.php

--- eliminar_confirmar.php
+++ eliminar_confirmar.php
@@ -1,5 +1,42 @@
 <?php
-    $id = $_GET['id'];
+    
+    //VERIFICAR SE O ID FOI INDICADO
+    //metodo 1, isset
+    // if(isset($_GET['id'])){
+    //     echo 'Existe';
+    // } else {
+    //     echo 'NAO EXISTE!';
+    // }
+    // die();
+    
+    //metodo 2, if not isset return to index
+    // if(!isset($_GET['id'])){
+    //     Header('Location:index.php');
+    // }
+
+    //metodo 3,if empty
+    // if(empty($_GET['id'])){
+    //     echo 'NÂO Existe';
+    //     die();
+    // } else {
+    //     echo 'EXISTE!';
+    // }
+
+    //metodo 4, filter-input - https://www.php.net/manual/pt_BR/function.filter-input.php
+    
+    $id = filter_input(INPUT_GET, 'id');
+
+    if($id == false){
+        echo 'não existe';
+        die();
+    }
+
+
+
+    //VERIFICAR SE EXISTE USER COM O ID INDICATO
+    
+
+    //$id = $_GET['id'];
     //echo $id;
     // apresentar dados do usuário
     include 'gestor.php';
