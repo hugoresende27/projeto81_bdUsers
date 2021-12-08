2c11aac4e832874e125956a1e4a491c642326cd2
hugoresende27 <Hugoresende27@gmail.com>
2021-12-08 12:32:12 +0000

    a

---
+++ html_footer.php
@@ -0,0 +1,34 @@
+</div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 text-center fundo-rodape p-3 fixed-bottom"><!--fixed-bottom rodapé sempre no fundo -->
+
+            Empresa &copy; 
+            <span class="ml-3 mr-3"> | <span class="ml-3 mr-3">
+                <a href="mailto:hugoresende27@gmail.com" class="link-email">hugoresende27@empresa.com</a>
+            <span class="ml-3 mr-3"> | <span class="ml-3 mr-3">
+                <a href="https://hugoresende27.github.io/portfolio/" target="_blank"><i class="fa fa-folder fa-2x" aria-hidden="true"></i></a>
+            
+            <a href="https://www.twitter.com" target="_blank" class="ml-4"><i class="fa fa-twitter-square fa-2x"></i></a>
+            
+            <a href="https://www.instagram.com" target="_blank" class="ml-4"><i class="fa fa-instagram fa-2x"></i></a>&nbsp;
+            
+            <?php   echo date('d-m-Y') ;//conteudo dinâmico, ano ('Y')
+                    echo '&nbsp&nbsp Horas: '; 
+                    echo date ('H:i:s') ;//Hora minutos e segundos
+            ?> 
+
+        
+        </div>
+    </div>
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    
+</body>
--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
+    include ('html_header.php');
+  
+    
 
     /////////////////////////////////////interface 
-    echo '<h3>gestão de users</h3>';
+   
+    echo "<h3 class='p-3 mb-2 bg-warning text-dark'>gestão de users</h3>";
     echo '<hr>';
 
 
     /////////////////////////////////////navegação
-    echo "<a href='form_novo_user.php'>Adicionar User</a>";
+    echo "<a href='form_novo_user.php' class='btn btn-secondary'>Adicionar User</a>";
     /////////////////////////////////// tabela com users existentes
     echo '<hr>';
 
@@ -22,23 +26,23 @@
     
 ?>
 
-<table border="1">
-    <thead>
+<table border="1" class="table-dark table-bordered table table-responsive">
+    <thead >
         <tr>
             <th>USER</th>
             <th>CRIADO EM:</th>
             <th>AÇÔES</th>
         </tr>
     </thead>
-    <tbody>
+    <tbody  class="text-left">
 
      <?php foreach($utilizadores as $usuario): ?> 
         <tr>
         <!-- em cada volta do ciclo cada $utilizador passa a ser $usuario  -->
             <td><?php echo ($usuario['user'])?></td>
             <td><?php echo ($usuario['created_at'])?></td>
-            <td><a href="editar_user.php?id=<?php echo $usuario['id_user'] ?>"> Editar </a>
-            &nbsp;| &nbsp; <a href="eliminar_confirmar.php?id=<?php echo $usuario['id_user'] ?>"> Eliminar </a></td>
+            <td><a href="editar_user.php?id=<?php echo $usuario['id_user'] ?>" class="btn btn-primary"> Editar </a>
+               <a href="eliminar_confirmar.php?id=<?php echo $usuario['id_user'] ?> "class="btn btn-danger"> Eliminar </a></td>
         </tr>
     <?php endforeach; ?>
 
@@ -46,3 +50,7 @@
 </table>
 
 <p>Resultados : <?php echo count($utilizadores) ?></p>
+
+<?php 
+    include ('html_footer.php');
+ 
